4671ea5b61ce0e8b1f9e999fda1d198d3b290e1f
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2014-06-23 12:32:26 -0300

    Added simple test

--- tests/NurseTests/DiTest.php
+++ tests/NurseTests/DiTest.php
@@ -4,7 +4,12 @@ namespace NurseTests;
 
 use PHPUnit_Framework_TestCase;
 
+use Nurse\Di;
+
 class DiTest extends PHPUnit_Framework_TestCase
 {
-
+    public function testCanLoadClass()
+    {
+        $this->assertInstanceOf('Nurse\Di', new Di);
+    }
 }
