f7ecde08cec03365a95067ded19b8ec59316cb7e
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2014-06-27 09:47:51 -0300

    Commented code

--- lib/Nurse/Container.php
+++ lib/Nurse/Container.php
@@ -6,18 +6,36 @@ class Container
 {
 
     private $definitions = array();
+
     private $data = array();
 
-    public function set($a, $b)
+    /**
+     * Defines the factory for the single instance objects that it should
+     * create on demand
+     *
+     * @param string $key the id for the callable function
+     * @param callable $callback the factory for the given key
+     * @return self
+     */
+    public function set($key, $callback)
     {
-        $this->definitions[$a] = $b;
+        $this->definitions[$key] = $callback;
     }
 
-    public function get($a)
+    /**
+     * Get the requested data. If the data is a callable function, then
+     * it only executes it the fist time and caches the result
+     *
+     * @param string $key the key for the
+     * @return mixed
+     * @throws InvalidArgumentException when requested key is not set
+     */
+    public function get($key)
     {
-        if (!isset($this->data[$a])) {
-            $this->data[$a] = $this->definitions[$a]();
+        if (!isset($this->data[$key])) {
+            $this->data[$key] = $this->definitions[$key]();
         }
-        return $this->data[$a];
+
+        return $this->data[$key];
     }
 }
